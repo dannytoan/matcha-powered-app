ba7c05bd0f114363066306eda248268f2ec4adb1
danny <dannytoan1@gmail.com>
2022-07-08 17:31:28 -0700

    added inclusive sizing to category seed

--- app/seeds/categories.py
+++ app/seeds/categories.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from app.models import db, Category
 
 def seed_categories():
@@ -6,6 +7,7 @@ def seed_categories():
     accessories = Category(id=3, category_name='accessories')
     supplements = Category(id=4, category_name='supplements')
     equipment = Category(id=5, category_name='equipment')
+    inclusive = Category(id=6, category_name='inclusive_sizing')
 
 
     db.session.add(womens)
@@ -13,6 +15,7 @@ def seed_categories():
     db.session.add(accessories)
     db.session.add(supplements)
     db.session.add(equipment)
+    db.session.add(inclusive)
 
     db.session.commit()
 
