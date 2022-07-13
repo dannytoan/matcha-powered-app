28b9b3126a3e2c175e67f089ad6ac2b36b865b44
danny <dannytoan1@gmail.com>
2022-07-12 17:47:06 -0700

    trying

--- react-app/src/components/ShoppingBag/index.js
+++ react-app/src/components/ShoppingBag/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromBag } from "../../store/shoppingBag";
-import { viewOrderHistory } from "../../store/orderHistory";
+import { viewOrderHistory } from "../../store/orderHistory.js";
 import "./ShoppingBag.css"
 
 
