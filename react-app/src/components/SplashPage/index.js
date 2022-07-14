1638f2987f749b882282f7ec3e231cb50835d46f
dannytoan <dannytoan1@gmail.com>
2022-07-14 09:52:01 -0700

    fixed error handling for login

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { viewProducts } from "../../store/products";
 import { demouser } from "../../store/session";
-import "./SplashPage.css"
-
+import "./SplashPage.css";
 
 function SplashPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
 
-
   const demoOnClick = async (e) => {
-    e.preventDefault()
-    await dispatch(demouser('demo@aa.io', 'password'));
-  }
+    e.preventDefault();
+    await dispatch(demouser("demo@aa.io", "password"));
+  };
 
   return (
     <div id="splash-page-container">
-        {/* <div>
+      {/* <div>
             <LoginForm />
         </div> */}
-        <div>
-            <h1 id="video-header-title" className="text">New <br/>Power, <br/> New <br/>Arrivals</h1>
-        </div>
+      <div id="h1-video-header-cntr">
+        <h1 id="video-header-title" className="text">
+          New
+          Power, New
+          Arrivals
+        </h1>
+      </div>
       <div id="video-header-container">
         <video id="video-header" autoPlay playsInline loop>
           <source
@@ -30,7 +32,16 @@ function SplashPage() {
             src="https://res.cloudinary.com/matchaprince/video/upload/v1657175755/trimmed_mountains_AdobeExpress_hqply0.mp4"
           />
         </video>
-        {sessionUser ? <></> : <button className="demo-btn" onClick={demoOnClick}>Demo User</button>}
+        <div>
+          <h2 className="text">Begin Exploring Now</h2>
+          {sessionUser ? (
+            <></>
+          ) : (
+            <button className="demo-btn" onClick={demoOnClick}>
+              Demo User
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
