a8888ef263e06adc58a0fc8b3ed24a850a0a723f
dannytoan <dannytoan1@gmail.com>
2022-08-10 14:43:31 -0700

    finished myshop page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -102,7 +102,7 @@ const NavBar = () => {
               <></>
             )}
             {sessionUser ? (
-              <Link to="/my-matchashark">
+              <Link to="/my-shop">
                 <li className="profile-btn-ctnr right-nav-icons">
                   <i class="fa-solid fa-user"></i>
                   <div className="text hide">PROFILE</div>
--- react-app/src/components/ProfileSidebar/index.js
+++ react-app/src/components/ProfileSidebar/index.js
@@ -4,29 +4,34 @@ import "./ProfileSideBar.css"
 function ProfileSideBar() {
     const currentUser = useSelector((state) => state.session.user)
     console.log("CURRENT USER", currentUser)
-
-
-    return (
-        <div id="profile-sidebar-cntr">
-            <div id="profile-top-header">
-
-              <img
-                id="profile-img-user-overview"
-                src={`https://ui-avatars.com/api/?name=${currentUser.first_name}&rounded=true&background=random&uppercase=false&size=40`}
-                alt="profile"
-                />
-              <div id="profile-greeting-text" className="text">Hi, <br/> <div id="profile-current-user-name" className="text">{currentUser.first_name}</div></div>
-                </div>
-            <div>
-
-                <li className="text profile-sidebar-category">Account Overview</li>
-                <li className="text profile-sidebar-category">My Shop</li>
-                <li className="text profile-sidebar-category">
-                <a href="/order-history" className="text">My Order History</a>
-                </li>
-            </div>
+  return (
+    <div id="profile-sidebar-cntr">
+      <div id="profile-top-header">
+        <img
+          id="profile-img-user-overview"
+          src={`https://ui-avatars.com/api/?name=${currentUser.first_name}&rounded=true&background=dbf4d7&uppercase=false&color=ffffff&size=40`}
+          alt="profile"
+        />
+        <div id="profile-greeting-text" className="text">
+          Hi, <br />{" "}
+          <div id="profile-current-user-name" className="text">
+            {currentUser.first_name}
+          </div>
         </div>
-    )
+      </div>
+      <div>
+        <li className="text profile-sidebar-category">Account Overview</li>
+        <a href="/my-shop" className="remove-link-dec">
+          <li className="text profile-sidebar-category">My Shop</li>
+        </a>
+        <li className="text profile-sidebar-category">
+          <a href="/order-history" className="text remove-link-dec">
+            My Order History
+          </a>
+        </li>
+      </div>
+    </div>
+  );
 }
 
 
